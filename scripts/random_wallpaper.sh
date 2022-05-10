66be49911cace598c1b3bec66d99f8772386b1cc
jdh <dongho971220@gmail.com>
2022-05-11 07:16:50 +0900

    remove unnecessary lines

--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-IMAGE=$(find ~/pics/wallpaper -type f | shuf -n1)
-
-feh --bg-center $IMAGE;
-
-WIDTH_PER_HEIGHT=$(identify -format '%w %h' $IMAGE | awk '{print $1/$2}')
+feh --bg-center $(find ~/pics/wallpaper -type f | shuf -n1)
