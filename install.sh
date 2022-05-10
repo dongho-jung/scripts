22276ca3fe4c7af83a6d0a36c2c04a65f49eb926
jdh <dongho971220@gmail.com>
2022-05-11 07:15:39 +0900

    convert tab to 4 space

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
 for file in $(dirname $(realpath $0))/scripts/*; do
-	ln -s `realpath $file` /usr/bin/`basename $file`
+    ln -s `realpath $file` /usr/bin/`basename $file`
 done
