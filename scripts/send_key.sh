2879dcff6b98a591d2f0db0c6585de9f4bb147d9
jdh <dongho971220@gmail.com>
2022-05-12 06:56:42 +0900

    use ID instead of CLASS, make it more robust

--- scripts/send_key.sh
+++ scripts/send_key.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "[!] usage: $0 WINDOW_CLASS KEYS"
+    echo "[!] usage: $0 WINDOW_ID KEYS"
     exit
 fi
 
 prev_window=`xdotool getactivewindow`
-current_desktop=`xdotool get_desktop`
 
-xdotool search --onlyvisible --class --desktop $current_desktop $1 windowactivate --sync key $2 windowactivate $prev_window
+xdotool windowactivate $1 sleep 0.1 key --clearmodifiers $2 windowactivate $prev_window
