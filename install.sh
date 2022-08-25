fddd7eb9e3343e7aa7038258f53f3da0a8346add
DongHo Jung <dongho971220@gmail.com>
2022-08-25 12:29:47 +0900

    remove unnecessary sudo logic

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-
 for file in $(dirname $(realpath $0))/scripts/*; do
     ln -s `realpath $file` /usr/local/bin/`basename $file`
 done
