4c24cd9136eec0517983e35c8af55fe6dae533a4
DongHo Jung <dongho971220@gmail.com>
2022-09-21 16:00:01 +0900

    use $HOME/.local/bin instead of /usr/local/bin so that normal user can install scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in $(dirname $(realpath $0))/scripts/*; do
-    ln -s `realpath $file` /usr/local/bin/`basename $file`
+    ln -s `realpath $file` $HOME/.local/bin/`basename $file`
 done
